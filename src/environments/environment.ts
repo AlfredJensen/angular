cb97dd37ef8cc40bad4ed271f8ad8b323d6551d9
AlfredJensen <ohhangzhoutoronto@gmail.com>
2022-06-01 01:39:46 +0800

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://localhost:5001",//   
+  apiUrl: "https://localhost:5002",//   
 };
 
 // export const environment = {
@@ -19,5 +19,6 @@ export const environment = {
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
+ * this is test for commint
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
